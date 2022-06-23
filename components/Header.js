96e88ba833b944516ec8e6bacc29467c6fc257fc
Ian McPhail <ian@clerk.dev>
2022-06-23 17:17:49 -0400

    feat: Update Header styles

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
-import common from '/styles/Common.module.css';
 import styles from '/styles/Header.module.css';
 
 const Header = () => {
@@ -15,17 +14,17 @@ const Header = () => {
       </Link>
       <SignedOut>
         {asPath !== '/sign-in' ? (
-          <p>
+          <p className={styles.p}>
             Been here before?{' '}
             <Link href="/sign-in">
-              <a className={common.link}>Sign in</a>
+              <a className={styles.link}>Sign in</a>
             </Link>
           </p>
         ) : (
-          <p>
+          <p className={styles.p}>
             New user?{' '}
             <Link href="/sign-up">
-              <a className={common.link}>Sign up</a>
+              <a className={styles.link}>Sign up</a>
             </Link>
           </p>
         )}
