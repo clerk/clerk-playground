e121ae7784e49ce36bb07bb17d8a7796072ef9ff
Ian McPhail <ian@clerk.dev>
2022-07-19 15:47:50 -0400

    feat: Add GitHub link to Header

--- components/Header.js
+++ components/Header.js
@@ -12,30 +12,39 @@ const Header = () => {
       <Link href="/">
         <a className={styles.logo}>Clerk Playground</a>
       </Link>
-      <SignedOut>
-        {asPath !== '/sign-in' ? (
-          <p className={styles.p}>
-            Been here before?{' '}
-            <Link href="/sign-in">
-              <a className={styles.link}>Sign in</a>
-            </Link>
-          </p>
-        ) : (
-          <p className={styles.p}>
-            New user?{' '}
-            <Link href="/sign-up">
-              <a className={styles.link}>Sign up</a>
-            </Link>
-          </p>
-        )}
-      </SignedOut>
-      <SignedIn>
-        <UserButton
-          userProfileURL="/user"
-          afterSignOutAll="/"
-          afterSignOutOneUrl="/"
-        />
-      </SignedIn>
+      <div className={styles.actions}>
+        <a
+          className={styles.button}
+          href="https://github.com/clerkinc/clerk-playground/"
+          target="_blank"
+        >
+          View on GitHub
+        </a>
+        <SignedOut>
+          {asPath !== '/sign-in' ? (
+            <p className={styles.p}>
+              Been here before?{' '}
+              <Link href="/sign-in">
+                <a className={styles.link}>Sign in</a>
+              </Link>
+            </p>
+          ) : (
+            <p className={styles.p}>
+              New user?{' '}
+              <Link href="/sign-up">
+                <a className={styles.link}>Sign up</a>
+              </Link>
+            </p>
+          )}
+        </SignedOut>
+        <SignedIn>
+          <UserButton
+            userProfileURL="/user"
+            afterSignOutAll="/"
+            afterSignOutOneUrl="/"
+          />
+        </SignedIn>
+      </div>
     </header>
   );
 };
